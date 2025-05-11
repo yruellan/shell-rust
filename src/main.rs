e07e6788d4919ebdd50636448b7b071052b21a4b
Yann <ruellan.yann@laposte.net>
2025-05-12 01:21:22 +0200

    Add exit 0

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+enum Result {
+    CommandNotFound,
+    Exit
+}
+
 fn main() {
     
     loop {
@@ -10,11 +15,28 @@ fn main() {
         // Wait for user input
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
-        run_cmd(input);
+        
+        match run_cmd(input) {
+            Result::Exit => {break},
+            _ => {}
+        }
     }
 }
 
-fn run_cmd(cmd: String){
+fn run_cmd(cmd_: String) -> Result {
 
-    println!("{}: command not found", cmd.trim());
+    let cmd = cmd_.trim();
+
+    // println!("Running command: {}, ({})", cmd, cmd == "exit 0");
+
+    match cmd {
+        "exit 0" => {
+            println!("Exiting...");
+            return Result::Exit;
+        },
+        _ => {
+            println!("{}: command not found", cmd.trim());
+            return Result::CommandNotFound;
+        }
+    }
 }
