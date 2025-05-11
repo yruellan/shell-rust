08bee24baa6b9e53c89adc6577a4a47ad99ce8e8
Yann <ruellan.yann@laposte.net>
2025-05-12 01:13:14 +0200

    loop for cmds

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,19 @@
 use std::io::{self, Write};
 
 fn main() {
-    // Uncomment this block to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+    
+        // Wait for user input
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        run_cmd(input);
+    }
+}
 
-    // Wait for user input
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    println!("{}: command not found", input.trim());
+fn run_cmd(cmd: String){
 
+    println!("{}: command not found", cmd.trim());
 }
